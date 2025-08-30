145caf1bf3d1785784b84c3f355e2e539c0973ee
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2025-08-30 20:11:33 +1000

    ok

--- commands.sh
+++ commands.sh
@@ -12,9 +12,10 @@ aws logs tail "/aws/lambda/ai-automation-course-payment-webhook" --since 48h --f
 
 
 
+aws dynamodb scan --table-name course_registrations --attributes-to-get "id" --query "Items[].id.S" --output text | tr '\t' '\n' | xargs -I {} aws dynamodb delete-item --table-name registration --key '{"id":{"S":"{}"}}'
+
+
+
 ppp  pytest test/integration/test_lambda_health.py -v   
 
-aws dynamodb scan \                                                       
---table-name course_registrations \
---query "Items[*].{Name:name.S, Email:email.S, PaymentStatus:payment_status.S}" \
---output table
+aws dynamodb scan --table-name course_registrations --query "Items[*].{Name:name.S, Email:email.S, PaymentStatus:payment_status.S}" --output table  
