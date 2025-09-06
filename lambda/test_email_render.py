497c3df1aed6ae71c81e32a7fdf6c544ed1f7c8b
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2025-09-06 13:11:55 +1000

    all working

---
+++ lambda/test_email_render.py
@@ -0,0 +1,10 @@
+import importlib.util
+spec = importlib.util.spec_from_file_location("livestream_handler", "livestream-handler.py")
+livestream_handler = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(livestream_handler)
+create_email_message = livestream_handler.create_email_message
+
+output = create_email_message("John Doe", "REG123456")
+
+with open("test_email_output.html", "w") as f:
+    f.write(output['Body']['Html']['Data'])
